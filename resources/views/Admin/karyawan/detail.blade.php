49b346800f7861e46f58f9b36dd30812b33b4615
annurrifki <annur.it2@gmail.com>
2017-11-09 07:20:20 +0700

    Kerjaan Henan

--- resources/views/Admin/karyawan/detail.blade.php
+++ resources/views/Admin/karyawan/detail.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
-                            <h2>Input Forms</h2>
+                            <h2>ID Card</h2>
                             <ul class="header-dropdown m-r--5">
                                 <li class="dropdown">
                                     <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
@@ -24,22 +24,45 @@
                                 </li>
                             </ul>
                         </div>
+                        <center>
                         <div class="body">
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <label class="form-label">Nama : {{ $karyawan->nama }}</label>
+                                    
+                                    <div style="height: 12cm; width: 10cm; background-image: url('/images/bg.jpg'); background-size: 100% 100%;  ">
+                                        <br>
+                                        <b><div style="color: white; font-size: 25px;">PERPUSTAKAAN BPI</div></b>
+                                        <br>
+                                        <div style="height: 8cm; width: 8cm; background-image: url('/images/avatar_karyawan.png'); background-size: 100% 100%;">
+                                        
                                     </div>
-                                </div>
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <label class="form-label">Jabatan : {{ $karyawan->jabatan }}</label>
+                                    <br>
+                                    <div style="color: white; font-size: 25px;"> {{ $karyawan->nama }}</div>
                                     </div>
-                                </div>
-                        
+                                    <div style="height: 4cm; width: 10cm; background-color: white">
+                                        <br>
+                                    <div style="color: #1E8BC3; font-size: 20px;"> {{ $karyawan->jabatan }}</div>
+                                        <br>
+                                        <div id="nik"></div>
+                                    
+                                    </div>
+                                
                         </div>
+                        </center>
                     </div>
                 </div>
             </div>
+
+
+@endsection
+@section('js')
+
+<script type="text/javascript">
+var table;
+      $(document).ready(function(){
+       
+       $("#nik").barcode("{{ $karyawan->nik }}", "ean13", { barHeight:50});     
+      
+});
+</script>
 
 
 @endsection
